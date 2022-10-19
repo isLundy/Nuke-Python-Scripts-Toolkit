21c1fad1306ea440e1b34741cf535de35b8a0202
Lundy Hu <lundyhu@outlook.com>
2022-10-19 10:16:38 +0800

    Add files via upload

--- nuLibrary/menu.py
+++ nuLibrary/menu.py
@@ -12,7 +12,7 @@
 
 import os
 
-version = '9.6.2'
+version = '9.6.3'
 year = '2022'
 releaseDate = 'October 18 {}'.format(year)
 print('nuLibrary v{}, built {}.\nCopyright (c) 2018-{} Lundy Hu.  All Rights Reserved.'.format(version, releaseDate, year))
