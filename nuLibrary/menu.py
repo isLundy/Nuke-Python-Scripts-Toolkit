7cc20840cc30a7b9dbf95fc037e314cf897ff70b
Lundy Hu <lundyhu@outlook.com>
2024-04-15 21:36:27 +0800

    Add files via upload

--- nuLibrary/menu.py
+++ nuLibrary/menu.py
@@ -8,4 +8,6 @@ import datetime
 version = 'v1.2'
 built_year = '2024'
 cur_year = datetime.datetime.now().year
+
+# Purely for entertainment
 print(f'nuLibrary {version}, built {built_year}.\nCopyright (c) 2018-{cur_year} Lundy Hu.  All Rights Reserved.')
